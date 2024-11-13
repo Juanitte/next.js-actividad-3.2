d388a01b706b357596db7733293724da1e145bae
Juanite <juan13_barsa@hotmail.com>
2024-11-13 15:21:16 +0100

    mucho texto añadido

--- src/app/1_2_1_condiciones_para_convalidar_fol/page.tsx
+++ src/app/1_2_1_condiciones_para_convalidar_fol/page.tsx
@@ -1,67 +1,183 @@
-import { HOME } from "@/utils/literals";
-import Image from "next/image";
-import tutorAndStudent from "@/app/img/tutor-and-student.png";
-import publicDomain from "@/app/img/public-domain.png";
+import { PAGE1_2_1 } from "@/utils/literals";
 import Titulo from "@/components/titulo";
 import TableComponent from "@/components/table-component";
 import { Table } from "@/model/Table";
 import { Tr } from "@/model/Tr";
 import { Td } from "@/model/Td";
 import Nav from "@/components/nav";
+import ItemLista from "@/components/item-lista";
 
 export default function Home() {
   return (
     <>
-      <Titulo>{HOME.titulo}</Titulo>
-      <Nav pos={0}/>
-      <div className="flex flex-row pb-2">
-        <div className="flex flex-col">
-          <p className="text-black px-4 max-w-[66vw] leading-tight text-justify">
-            {HOME.p1}
+      <Titulo>{PAGE1_2_1.titulo}</Titulo>
+      <Nav pos={6}/>
+      <div className="flex flex-col">
+        <div className="flex flex-col text-sm">
+          <p className="text-black px-4 leading-tight text-justify pb-4">
+            {PAGE1_2_1.p1}
           </p>
-          <p className="text-black pt-3 px-4">
-            {HOME.p2}
-          </p>
-        </div>
-        <div className="flex flex-col">
-          <Image src={tutorAndStudent} alt="tutor and student" width={140} height={140} className="pt-2 pl-4" />
-          <div className="flex flex-row justify-between items-center ">
-            <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.es" className="pt-2">
-              <Image src={publicDomain} alt="public domain" width={70} height={60} />
-            </a>
-            <div className="flex flex-row pt-2 pl-1">
-              <p className="text-black text-[9px]">
-                {HOME.imgLink1}
+          <ItemLista interior={false}>
+            <p className="text-black px-4 leading-tight text-justify">
+              {PAGE1_2_1.p2}
+              <span className="font-bold">
+                {PAGE1_2_1.pBold1}
+              </span>
+            </p>
+            <ItemLista interior={true}>
+              <p className="text-black px-4 leading-tight text-justify">
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold2}
+                </span>
               </p>
-              <a href="https://openclipart.org/detail/194249/tutor-and-student" className="text-blue-500 text-[9px] pl-1 underline">
-                {HOME.imgLink2}
-              </a>
-            </div>
-          </div>
+            </ItemLista>
+            <ItemLista interior={true}>
+              <p className="text-black px-4 leading-tight text-justify">
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold3}
+                </span>
+                {PAGE1_2_1.p3}
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold4}
+                </span>
+              </p>
+            </ItemLista>
+            <ItemLista interior={true}>
+              <p className="text-black px-4 leading-tight text-justify">
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold5}
+                </span>
+              </p>
+            </ItemLista>
+          </ItemLista>
+        </div>
+        <div className="flex flex-col text-sm pt-4">
+          <ItemLista interior={false}>
+            <p className="text-black px-4 leading-tight text-justify">
+              {PAGE1_2_1.p4}
+            </p>
+            <ItemLista interior={true}>
+              <p className="text-black px-4 leading-tight text-justify">
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold6}
+                </span>
+              </p>
+            </ItemLista>
+            <ItemLista interior={true}>
+              <p className="text-black px-4 leading-tight text-justify">
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold7}
+                </span>
+              </p>
+            </ItemLista>
+          </ItemLista>
+        </div>
+        <div className="flex flex-col text-sm py-4">
+          <ItemLista interior={false}>
+            <p className="text-black px-4 leading-tight text-justify">
+              {PAGE1_2_1.p5}
+            </p>
+            <ItemLista interior={true}>
+              <p className="text-black px-4 leading-tight text-justify">
+                <span className="font-bold">
+                  {PAGE1_2_1.pBold8}
+                </span>
+              </p>
+            </ItemLista>
+          </ItemLista>
         </div>
       </div>
-      <TableComponent table={new Table(
-        [
-          new Tr(
-            [
-              new Td(`${HOME.elaboradoTitulo}`, 10, "vw", 1, 1, true),
-              new Td(`${HOME.elaboradoContenido}`, 50, "vw", 1, 1, false)
-            ]
-          ),
-          new Tr(
-            [
-              new Td(`${HOME.versionTitulo}`, 10, "vw", 1, 1, true),
-              new Td(`${HOME.versionContenido}`, 50, "vw", 1, 1, false)
-            ]
-          ),
-          new Tr(
-            [
-              new Td(`${HOME.fechaTitulo}`, 10, "vw", 1, 1, true),
-              new Td(`${HOME.fechaContenido}`, 50, "vw", 1, 1, false)
-            ]
-          )
-        ]
-      )} />
+      <div className="text-sm">
+        <TableComponent table={new Table(
+          [
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table1}`, 25, "vw", 1, 1, true),
+                new Td(`${PAGE1_2_1.table2}`, 40, "vw", 1, 1, true)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table3}`, 25, "vw", 1, 4, true),
+                new Td(`${PAGE1_2_1.table4}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table5}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table6}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table7}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table8}`, 25, "vw", 1, 1, true),
+                new Td(`${PAGE1_2_1.table9}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table10}`, 25, "vw", 1, 5, true),
+                new Td(`${PAGE1_2_1.table11}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table12}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table13}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table14}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table15}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table16}`, 25, "vw", 1, 5, true),
+                new Td(`${PAGE1_2_1.table17}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table18}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table19}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table20}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_1.table21}`, 40, "vw", 1, 1, false)
+              ]
+            )
+          ]
+        )} />
+      </div>
     </>
   );
 }
--- src/app/1_2_2_condiciones_para_convalidar_empresa_e_iniciativa_emprendedora/page.tsx
+++ src/app/1_2_2_condiciones_para_convalidar_empresa_e_iniciativa_emprendedora/page.tsx
@@ -1,67 +1,163 @@
-import { HOME } from "@/utils/literals";
-import Image from "next/image";
-import tutorAndStudent from "@/app/img/tutor-and-student.png";
-import publicDomain from "@/app/img/public-domain.png";
+import { PAGE1_2_2 } from "@/utils/literals";
 import Titulo from "@/components/titulo";
 import TableComponent from "@/components/table-component";
 import { Table } from "@/model/Table";
 import { Tr } from "@/model/Tr";
 import { Td } from "@/model/Td";
 import Nav from "@/components/nav";
+import ItemLista from "@/components/item-lista";
 
 export default function Home() {
   return (
     <>
-      <Titulo>{HOME.titulo}</Titulo>
-      <Nav pos={0}/>
-      <div className="flex flex-row pb-2">
-        <div className="flex flex-col">
-          <p className="text-black px-4 max-w-[66vw] leading-tight text-justify">
-            {HOME.p1}
+      <Titulo>{PAGE1_2_2.titulo}</Titulo>
+      <Nav pos={7}/>
+      <div className="flex flex-col">
+        <div className="flex flex-col text-sm">
+          <p className="text-black px-4 leading-tight text-justify pb-4">
+            {PAGE1_2_2.p1}
+            <span className="font-bold">
+              {PAGE1_2_2.pBold1}
+            </span>
+            {PAGE1_2_2.p2}
           </p>
-          <p className="text-black pt-3 px-4">
-            {HOME.p2}
-          </p>
+          <ItemLista interior={false}>
+            <p className="text-black px-4 leading-tight text-justify">
+              {PAGE1_2_2.p3}
+              <span className="font-bold">
+                {PAGE1_2_2.pBold2}
+              </span>
+              {PAGE1_2_2.p4}
+            </p>
+          </ItemLista>
         </div>
-        <div className="flex flex-col">
-          <Image src={tutorAndStudent} alt="tutor and student" width={140} height={140} className="pt-2 pl-4" />
-          <div className="flex flex-row justify-between items-center ">
-            <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.es" className="pt-2">
-              <Image src={publicDomain} alt="public domain" width={70} height={60} />
-            </a>
-            <div className="flex flex-row pt-2 pl-1">
-              <p className="text-black text-[9px]">
-                {HOME.imgLink1}
-              </p>
-              <a href="https://openclipart.org/detail/194249/tutor-and-student" className="text-blue-500 text-[9px] pl-1 underline">
-                {HOME.imgLink2}
-              </a>
-            </div>
-          </div>
+        <div className="flex flex-col text-sm pt-4">
+          <ItemLista interior={false}>
+            <p className="text-black px-4 leading-tight text-justify">
+              {PAGE1_2_2.p5}
+              <span className="font-bold">
+                {PAGE1_2_2.pBold3}
+              </span>
+              {PAGE1_2_2.p6}
+            </p>
+          </ItemLista>
+        </div>
+        <div className="flex flex-col text-sm py-4">
+          <ItemLista interior={false}>
+            <p className="text-black px-4 leading-tight text-justify">
+              <span className="font-bold">
+                {PAGE1_2_2.pBold4}
+              </span>
+            </p>
+          </ItemLista>
         </div>
       </div>
-      <TableComponent table={new Table(
-        [
-          new Tr(
-            [
-              new Td(`${HOME.elaboradoTitulo}`, 10, "vw", 1, 1, true),
-              new Td(`${HOME.elaboradoContenido}`, 50, "vw", 1, 1, false)
-            ]
-          ),
-          new Tr(
-            [
-              new Td(`${HOME.versionTitulo}`, 10, "vw", 1, 1, true),
-              new Td(`${HOME.versionContenido}`, 50, "vw", 1, 1, false)
-            ]
-          ),
-          new Tr(
-            [
-              new Td(`${HOME.fechaTitulo}`, 10, "vw", 1, 1, true),
-              new Td(`${HOME.fechaContenido}`, 50, "vw", 1, 1, false)
-            ]
-          )
-        ]
-      )} />
+      <div className="text-sm">
+        <TableComponent table={new Table(
+          [
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table1}`, 25, "vw", 1, 1, true),
+                new Td(`${PAGE1_2_2.table2}`, 40, "vw", 1, 1, true)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table3}`, 25, "vw", 1, 15, true),
+                new Td(`${PAGE1_2_2.table4}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table5}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table6}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table7}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table8}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table9}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table10}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table11}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table12}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table13}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table14}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table15}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table16}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table17}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table18}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table19}`, 25, "vw", 1, 1, true),
+                new Td(`${PAGE1_2_2.table20}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table21}`, 25, "vw", 1, 1, true),
+                new Td(`${PAGE1_2_2.table22}`, 40, "vw", 1, 1, false)
+              ]
+            ),
+            new Tr(
+              [
+                new Td(`${PAGE1_2_2.table23}`, 25, "vw", 1, 1, true),
+                new Td(`${PAGE1_2_2.table24}`, 40, "vw", 1, 1, false)
+              ]
+            )
+          ]
+        )} />
+      </div>
     </>
   );
 }
